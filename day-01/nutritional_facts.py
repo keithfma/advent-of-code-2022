dd1d9427939bc5224943f3deca01b171e2cfda14
Keith Ma <keithfma@gmail.com>
2022-12-01 22:00:48 -0500

    Solution for day 1 part 2

--- day-01/nutritional_facts.py
+++ day-01/nutritional_facts.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 
-INPUT = Path('part-1-input.txt')
+INPUT = Path('input.txt')
 
 
 def max_calories_carried_by_one_elf(food_list_path):
@@ -28,6 +28,31 @@ def max_calories_carried_by_one_elf(food_list_path):
     return max_elf_calories
 
 
+def top_calories_carried(food_list_path):
+    """Return the sum of calories carried by the 3 elves with the most calories
+    """
+    
+    max_elf_calories = [-1, -1, -1]
+    elf_calories = 0
+
+    with open(food_list_path, 'r') as fp:
+
+        for line in fp.readlines():
+
+            if line == '\n':
+                # new elf!
+                if elf_calories > max_elf_calories[0]:
+                    max_elf_calories = sorted([elf_calories, *max_elf_calories[1:]])
+                elf_calories = 0  
+
+            else:
+                elf_calories += int(line)
+
+    return sum(max_elf_calories)
+
+
+
 if __name__ == '__main__':
 
     print(f'Maximum calories carried by any elf: {max_calories_carried_by_one_elf(INPUT)}')
+    print(f'Total calories carried by top-3 elves: {top_calories_carried(INPUT)}')
